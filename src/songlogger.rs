af41940e1ff9b85b806dd3fb8391e125e7058ea2
Esa Varemo <git@esav.fi>
2021-11-06 16:05:20 +0200

    Parse song details from response

--- src/songlogger.rs
+++ src/songlogger.rs
@@ -5,6 +5,19 @@ use reqwest::{blocking::Client, header::AUTHORIZATION};
 
 use crate::AuthInfo;
 
+use serde::Deserialize;
+use serde_json;
+
+#[derive(Debug,Deserialize)]
+struct Song {
+    name: String,
+}
+
+#[derive(Debug,Deserialize)]
+struct SpotifyResponse {
+    item: Song,
+}
+
 pub fn run(auth: AuthInfo) {
     println!("Started logger with: {:?}", auth);
 
@@ -15,7 +28,9 @@ pub fn run(auth: AuthInfo) {
             .get("https://api.spotify.com/v1/me/player/currently-playing")
             .header(AUTHORIZATION, format!("Bearer {}", auth.access_token.access_token().secret()))
             .send().expect("Failed to call spotify API");
-        println!("{:?}", res.text());
+        let body = res.text().unwrap();
+        let parsed: SpotifyResponse = serde_json::from_str(&body).expect("Failed to parse JSON");
+        println!("{:?}", parsed);
 
         thread::sleep(Duration::new(1, 0));
     }
